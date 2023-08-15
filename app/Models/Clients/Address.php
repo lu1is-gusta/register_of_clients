a591c88bc2398fe671423537ba68a31e4fa984a9
Luís Gustavo <gustavovacodar@gmail.com>
2023-08-15 20:34:02 -0300

    create migrations and models

---
+++ app/Models/Clients/Address.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Clients;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Clients\Client;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['type_of_address', 'road', 'number', 'city', 'state', 'country'];
+
+    public function client(){
+        return $this->belongsTo(Client::class);
+    }
+}
---
+++ app/Models/Clients/Client.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Clients;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Clients\Address;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'telephone', 'date_of_birth', 'cpf'];
+
+    function address(){
+        return $this->hasMany(Address::class);
+    }
+}
