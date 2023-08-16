cb13e5d22b479e57fe62a25a73e65e64b5909159
Luís Gustavo <gustavovacodar@gmail.com>
2023-08-15 23:50:21 -0300

    create controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', ClientController::class);
